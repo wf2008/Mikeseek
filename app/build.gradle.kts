61bec66ad97233a3a6d0e5c6dbfd4ef158944309
wf2008 <michealchukwuemeka2008@gmail.com>
2026-06-02 23:53:28 +0100

    Ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "wfseek.app"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
